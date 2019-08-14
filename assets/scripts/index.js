c0ed18ce6d52bf503cbb2d009bb601902e673c97
britneyart80 <chen.bri@husky.neu.edu>
2019-08-14 16:40:28 -0400

    Transferred styling over to new temp

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,29 @@
 $(() => {
+  // smooth transition on nav link click
+  $('a.toscroll').on('click', function (e) {
+    const url = e.target.href
+    const hash = url.substring(url.indexOf('#') + 1)
+    $('html, body').animate({
+      scrollTop: $('#' + hash).offset().top
+    }, 1300)
+    return false
+  })
 
+  // back to top button
+  const btn = $('#back-to-top')
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show')
+    } else {
+      btn.removeClass('show')
+    }
+  })
+
+  btn.on('click', function (e) {
+    e.preventDefault()
+    $('html, body').animate({
+      scrollTop: 0
+    }, '300')
+  })
 })
