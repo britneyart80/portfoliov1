dbf5601f4e16ab35fe99e21b3ed008b3212be707
britneyart80 <chen.bri@husky.neu.edu>
2020-06-08 21:34:39 -0700

    implemented page scroll fade in effect

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -31,7 +31,27 @@ $(() => {
     const name = $(this).attr('name')
     $('p', this).html(name)
   })
+
   $('i').mouseout(function () {
     $('p', this).html('')
   })
+
+  $(window).scroll(function() {
+    // var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    // var windowTop = $(this).scrollTop();
+
+    $(".fade-scroll").each(function() {
+      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+      var windowTop = $(window).scrollTop();
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      var objectTop = $(this).offset().top + $(this).outerHeight() - $(this).height();
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom - 150 < windowTop || objectTop + 100 > windowBottom) { //object comes into view
+        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      } else { //object goes out of view (scrolling up)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
 })
